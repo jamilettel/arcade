9c4df1e3a7e603625cda5279540a60b42c13ff1a
Jamil ETTEL <jamil.ettel@epitech.eu>
2020-03-09 16:39:01 +0100

    Utils.hpp updated + added const map to SfmlGraphical

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -105,6 +105,16 @@ namespace arc {
             MOUSE_1,
             MOUSE_2,
             MOUSE_3,
+            NUM1,
+            NUM2,
+            NUM3,
+            NUM4,
+            NUM5,
+            NUM6,
+            NUM7,
+            NUM8,
+            NUM9,
+            NUM0,
         };
 
 
