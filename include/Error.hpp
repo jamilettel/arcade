eba92aae54a0c60818bdc4e7baaf87efbcb5900c
Benjamin Bourgeois <benjishift@gmail.com>
2020-03-26 18:33:16 +0100

    {DOCS} Error class

--- include/Error.hpp
+++ include/Error.hpp
@@ -5,6 +5,14 @@
 ** Error
 */
 
+/**
+ * \file        Error.hpp
+ * \author      Amaury Lecomte - Benjamin Bourgeois - Celeste Bousseau - Jamil Ettel - Oriane Aumoitte
+ * \date        March 8 2020
+ * \brief       Error Arcade program
+ * \details     ArcadeError are used by the core to detect error.
+ */
+
 #ifndef ERROR_HPP_
 #define ERROR_HPP_
 
@@ -12,16 +20,34 @@
 #include <string>
 
 namespace arc {
-
+    /**
+     * \class ArcadeError
+     * \brief This class is the main error class in the Arcade program. All other class error must be derived from this class
+     */
     class ArcadeError: public std::exception {
     public:
+        /**
+         *  \brief Constructor
+         *  @param component Component's name in which the error appears
+         *  @param message Error message
+         */
         ArcadeError(const std::string &component, const std::string &message);
 
+        /**
+         *  \brief Get the error message
+         *  @return Error message
+         */
         const char *what() const noexcept override;
+        /**
+         *  \brief Get the name of the component in error
+         *  @return Name of the component in error
+         */
         const std::string &getComponent() const noexcept;
 
     private:
+        /// Component's name in which the error appears.
         std::string _component;
+        /// Error message.
         std::string _message;
     };
 
